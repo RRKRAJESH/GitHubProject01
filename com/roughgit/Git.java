87849bbc8ccf044ded471c7fad517788d7fbf1c5
RRKRAJESH <kumarrajesh984000@gmail.com>
2022-12-03 12:45:38 +0530

    RoughGit

---
+++ com/roughgit/Git.java
@@ -0,0 +1,23 @@
+package com.roughgit;
+
+public class Git {
+	
+	private void method() {
+
+		System.out.println(100);
+		
+		System.out.println(200);
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Git a = new Git();	
+		
+		System.out.println(500);
+		
+	}
+
+}
